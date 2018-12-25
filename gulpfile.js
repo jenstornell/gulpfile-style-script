4cde358519f8eb6cdff86a866522c1c2c1214759
Jens Törnell <jens.tornell@gmail.com>
2018-12-25 15:42:22 +0100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    concat = require("gulp-concat");
+
+let uglify = require('gulp-uglify-es').default;
+
+var paths = {
+    styles: {
+        src: "assets/css/src/**/*.scss",
+        dest: "assets/css/dest"
+    },
+    scripts: {
+        src: "assets/js/src/**/*.js",
+        dest: "assets/js/dest"
+    }
+};
+
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(concat('test.scss'))
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(autoprefixer())
+            .pipe(gulp.dest(paths.styles.dest))
+    );
+}
+
+function script() {
+    return (
+        gulp
+            .src(paths.scripts.src)
+            .pipe(concat('script.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest(paths.scripts.dest))
+    );
+}
+
+function watch(){
+    gulp.watch(paths.styles.src, style);
+    gulp.watch(paths.scripts.src, script);
+}
+
+exports.style = style;
+exports.script = script;
+exports.watch = watch;
